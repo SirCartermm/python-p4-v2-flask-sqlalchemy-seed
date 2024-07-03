596f66630458198da2b4c8f25d336b1ad4d65023
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-07-03 17:13:03 +0300

    modified seed.py

--- server/seed.py
+++ server/seed.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 # server/seed.py
 
+from random import choice as rc
+from faker import Faker
+
 from app import app
 from models import db, Pet
 
 with app.app_context():
+    fake = Faker()
+    Pet.query.delete()
     pets = []
-    pets.append(Pet(name="Fido", species="Dog"))
-    pets.append(Pet(name="Whiskers", species="Cat"))
-    pets.append(Pet(name="Hermie", species="Hamster"))
+    species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
+    for n in range(10):
+        pet = Pet(name=fake.first_name(), species=rc(species))
+        pets.append(pet)
     db.session.add_all(pets)
     db.session.commit()
